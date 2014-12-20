c28afec00868af600bb74d350b2e8f0b14daf30e
Shuwen SUN <chris.void.92@gmail.com>
2014-12-20 23:47:12 +0800

    S

---
+++ leetcode/max_subarray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+class Solution 
+{
+public:
+    int maxSubArray(int A[], int n)
+        {
+            
+        }
+
+};
+
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
+
+
+
+
+//#For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
+//    the contiguous subarray [4,−1,2,1] has the largest sum = 6.
--- topcoder/BinarySearchable.cpp
+++ topcoder/BinarySearchable.cpp
@@ -38,9 +38,9 @@ public:
             if ( (Case == -1) || (Case == 1) )
                 test_case_1;
             if ( (Case == -1) || (Case == 2) )
-                test_case_2;
+                 test_case_2;
             if ( (Case == -1) || (Case == 3) )
-                test_case_3;
+                 test_case_3;
             if ( (Case == -1) || (Case == 4) )
                 test_case_4;
         }
@@ -50,7 +50,8 @@ private:
         {
             ostringstream os;
             os << "{";
-            for (typename vector <T>::const int &Expected, const int &Received){
+            for (typename vector <T>::const int ){
+                
                 
 
 
